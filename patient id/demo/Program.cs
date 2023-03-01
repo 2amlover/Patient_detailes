408666f37d2268065631b5588ba0591f91103671
Mohan <www.insultu@gmail.com>
2023-03-01 15:49:46 +0530

    Version 1.1

---
+++ patient id/demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("WAIT FOR API RESPONSE....!");
+           
+            string url = "http://ganga.pihms.co.in/Patient/get_PatientByID";
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.PostAsync(url, new StringContent("{\"m_PatientID\":\"114378\"}", Encoding.UTF8, "application/json"));
+
+            string responseresult = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseresult);
+            Console.ReadLine();
+        }
+
+    }
+}
